7c4d6051189201e69756912a1c9f592b38c838a3
Ashutosh Shaha <ashutoshbshaha30@gmail.com>
2020-07-14 20:18:26 +0530

    Deployment

--- public/js/app.js
+++ public/js/app.js
@@ -12,7 +12,7 @@ weatherForm.addEventListener('submit', (e) => {
     msg1.textContent = 'Loading...'
     msg2.textContent = ''
 
-    fetch('http://localhost:3000/weather?address='+location).then((response) => {
+    fetch('/weather?address='+location).then((response) => {
     response.json().then((data) => {
         if(data.error) {
             msg1.textContent = data.error
